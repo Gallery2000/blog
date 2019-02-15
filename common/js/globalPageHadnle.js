a9635af0bf0b0db095c50aa49adc3071f90fd425
Gallery2000 <13268793291@163.com>
2019-02-15 13:53:48 +0800

    9

---
+++ common/js/globalPageHadnle.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    start();
+    $(window).on('scroll', function () {
+        start()
+    })
+    function start() {
+        //.not('[data-isLoaded]')选中已加载的图片不需要重新加载
+        $('img').not('[data-isLoaded]').each(function () {
+            var $node = $(this)
+            if (isShow($node)) {
+                loadImg($node)
+            }
+        })
+    }
+
+    function isShow($node) {
+        return $node.offset().top <= $(window).height() + $(window).scrollTop()
+    }
+    function loadImg($img) {
+
+        $img.attr('src', $img.attr('data-src'))
+        $img.attr('data-isLoaded', 1)
+    }
+    $('.hamburger').click(function(){
+        if($('.phone-navbar').hasClass('active')){
+            $('.phone-navbar').removeClass('active');
+        }else{
+            $('.phone-navbar').addClass('active');
+        }
+    })
+    $('.full-screen').find('a').on('click',function(){
+        var pathname = new RegExp(location.pathname);
+        var name =  $(this).attr('data-name');
+        if(!(/index/.test(pathname))){
+            window.location.href = '../index/index.html#'+name;
+        }else{
+            location.hash = "";
+            location.hash = name;
+        }
+        $('.phone-navbar').removeClass('active');
+    })
+})
