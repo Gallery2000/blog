da7e1a1f9ab9fe2d5f41347d0115b020a1fceb5a
Gallery2000 <13268793291@163.com>
2019-02-22 16:05:34 +0800

    no message

--- common/js/globalPageHadnle.js
+++ common/js/globalPageHadnle.js
@@ -28,17 +28,17 @@ $(document).ready(function () {
             $('.phone-navbar').addClass('active');
         }
     })
-    $('.full-screen').find('a').on('click',function(){
-        var pathname = new RegExp(location.pathname);
-        var name =  $(this).attr('data-name');
-        if(!(/index/.test(pathname))){
-            window.location.href = '../index/index.html#'+name;
+    $('.phone-navbar').on('click','.icon',function(){
+        if($('.phone-navbar').hasClass('active')){
+            $('.phone-navbar').removeClass('active');
         }else{
-            var top = (parseInt($('html').css('font-size'))*130)/100;
-            $('html,body').scrollTop($('#'+name).offset().top-top);
-            // location.hash = "";
-            // location.hash = name;
+            $('.phone-navbar').addClass('active');
+
         }
+    })
+    $('.full-screen').find('a').on('click',function(){
         $('.phone-navbar').removeClass('active');
+        // slidepage.slideTo(parseInt($(this).attr('data-index'))+1);
+        location.href = '../mindex/mindex.html#'+$(this).attr('data-name');
     })
 })
